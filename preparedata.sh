0f2c3c79fc8e31fc825a33bc28464bfdfff74dec
Rholais Lii <rholais@gmail.com>
2021-02-04 14:03:01 -0800

    Update preparedata.sh

--- preparedata.sh
+++ preparedata.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 dir=ft-drv-grammar
-rm -rf data-${dir}
-mkdir data-${dir}
-for i in split-extract-${dir}/*; do
-    cp -f $i/`basename $i`"_M"*/*.{drv,sel,snk}.csv ./data-${dir}/
+rm -rf data
+mkdir data
+for i in split-extract-data/*; do
+    cp -f $i/`basename $i`"_M"*/*.{drv,sel,snk}.csv ./data
 done
-
